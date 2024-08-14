7f47290804795ff542229bca685aed5e5815208d
wugu <2231597633@qq.com>
2024-08-14 21:01:21 +0800

    数组中心下标

---
+++ src/ArrayCenterIndex.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArrayCenterIndex { // 求数组的中心下标 左边元素和=右边元素和
+    public static void main(String[] args) {
+        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+    }
+
+    private static int pivotIndex(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+            if (total == sum){
+                return i;
+            }
+            sum = sum - nums[i];
+        }
+        return -1;
+    }
+}
